5d43efeca807e6d7477eaa1ca5374acd7ae1d6c4
김태현 <taehyk528@gmail.com>
2022-07-22 17:04:13 +0900

    [#2] 프로젝트 생성

---
+++ Tamagotchi/Tamagotchi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tamagotchi
+//
+//  Created by taekki on 2022/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
