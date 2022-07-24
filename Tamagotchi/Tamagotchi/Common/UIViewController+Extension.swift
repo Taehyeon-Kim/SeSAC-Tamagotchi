c52bb283f663c8cb2db01a15a30dac38363b300f
김태현 <taehyk528@gmail.com>
2022-07-25 01:48:18 +0900

    [#16] Alert 익스텐션 수정

--- Tamagotchi/Tamagotchi/Common/UIViewController+Extension.swift
+++ Tamagotchi/Tamagotchi/Common/UIViewController+Extension.swift
@@ -11,10 +11,14 @@ extension UIViewController {
     func makeAlert(
         title: String,
         message: String?,
-        cancelAction: UIAlertAction,
-        confirmAction: UIAlertAction
+        cancelTitle: String,
+        confirmTitle: String,
+        cancelHandler: (() -> Void)? = nil,
+        confirmHandler: (() -> Void)? = nil
     ) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: cancelTitle, style: .cancel) { _ in cancelHandler?() }
+        let confirmAction = UIAlertAction(title: confirmTitle, style: .default) { _ in confirmHandler?() }
         alertController.addAction(cancelAction)
         alertController.addAction(confirmAction)
         self.present(alertController, animated: true)
@@ -23,9 +27,11 @@ extension UIViewController {
     func makeAlert(
         title: String,
         message: String?,
-        confirmAction: UIAlertAction
+        confirmTitle: String,
+        confirmHandler: (() -> Void)? = nil
     ) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let confirmAction = UIAlertAction(title: confirmTitle, style: .default) { _ in confirmHandler?() }
         alertController.addAction(confirmAction)
         self.present(alertController, animated: true)
     }
