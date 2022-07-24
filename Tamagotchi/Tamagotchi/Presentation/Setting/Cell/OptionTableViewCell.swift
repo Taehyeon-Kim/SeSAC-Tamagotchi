08b1ca16b36e8cd2e8936dca9bfd2ab2b79f94ff
김태현 <taehyk528@gmail.com>
2022-07-24 23:57:04 +0900

    [#12] 설정 화면 셀 레이아웃

---
+++ Tamagotchi/Tamagotchi/Presentation/Setting/Cell/OptionTableViewCell.swift
@@ -0,0 +1,11 @@
+//
+//  OptionTableViewCell.swift
+//  Tamagotchi
+//
+//  Created by taekki on 2022/07/24.
+//
+
+import UIKit
+
+final class OptionTableViewCell: UITableViewCell {
+}
--- Tamagotchi/Tamagotchi/Presentation/Setting/SettingViewController.swift
+++ Tamagotchi/Tamagotchi/Presentation/Setting/SettingViewController.swift
@@ -7,9 +7,23 @@
 
 import UIKit
 
-final class SettingViewController: UIViewController {
+final class SettingViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension SettingViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "OptionTableViewCell") as? OptionTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        return cell
     }
 }
