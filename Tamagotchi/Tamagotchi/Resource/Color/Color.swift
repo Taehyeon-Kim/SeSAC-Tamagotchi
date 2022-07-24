ee9d5ddbec85ab3bf393e1e8be924c5c9f8a9aea
김태현 <taehyk528@gmail.com>
2022-07-24 22:04:46 +0900

    [#9] 컬러 세팅

---
+++ Tamagotchi/Tamagotchi/Resource/Color/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  Tamagotchi
+//
+//  Created by taekki on 2022/07/24.
+//
+
+import UIKit
+
+enum Color {
+    static let backgroundColor = UIColor(red: 245/255, green: 252/255, blue: 252/255, alpha: 1)
+    static let pointColor = UIColor(red: 77/255, green: 106/255, blue: 120/255, alpha: 1)
+}
