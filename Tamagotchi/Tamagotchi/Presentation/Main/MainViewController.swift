2646f69d1ee984f0947bdc566483779476321fdc
김태현 <taehyk528@gmail.com>
2022-07-24 23:48:52 +0900

    [#11] 메인 화면 레이아웃 구현 (디테일 제외)

--- Tamagotchi/Tamagotchi/Presentation/Main/MainViewController.swift
+++ Tamagotchi/Tamagotchi/Presentation/Main/MainViewController.swift
@@ -8,8 +8,55 @@
 import UIKit
 
 final class MainViewController: UIViewController {
+    
+    var nickname: String?
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: CustomPaddingLabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var riceTextField: UITextField!
+    @IBOutlet weak var riceButton: UIButton!
+    @IBOutlet weak var waterDropTextField: UITextField!
+    @IBOutlet weak var waterDropButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureUI()
+        self.configureNavigationBar()
+    }
+}
+
+extension MainViewController {
+    private func configureUI() {
+        self.view.backgroundColor = Color.backgroundColor
+        self.messageLabel.textColor = Color.pointColor
+        self.nameLabel.textColor = Color.pointColor
+        self.nameLabel.backgroundColor = Color.backgroundColor
+        self.nameLabel.layer.borderColor = Color.pointColor.cgColor
+        self.nameLabel.layer.borderWidth = 1
+        self.nameLabel.layer.cornerRadius = 4
+        self.nameLabel.padding(top: 0, bottom: 0, left: 6, right: 6)
+        self.statusLabel.textColor = Color.pointColor
+        self.riceTextField.placeholder = "밥주세요"
+        self.waterDropTextField.placeholder = "물주세요"
+        [self.riceTextField, self.waterDropTextField].forEach {
+            $0?.textAlignment = .center
+        }
+        [self.riceButton, self.waterDropButton].forEach {
+            $0?.setTitleColor(Color.pointColor, for: .normal)
+            $0?.layer.borderColor = Color.pointColor.cgColor
+            $0?.layer.borderWidth = 1
+            $0?.layer.cornerRadius = 4
+        }
+    }
+    
+    private func configureNavigationBar() {
+        self.title = "\(nickname ?? "대장")님의 다마고치"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.circle"), style: .plain, target: self, action: #selector(transitionToSettingViewController))
+    }
+    
+    @objc func transitionToSettingViewController() {
+        
     }
 }
