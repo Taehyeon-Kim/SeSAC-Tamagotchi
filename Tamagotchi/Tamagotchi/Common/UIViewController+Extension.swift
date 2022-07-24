7eed16fae0a82080e55e65f0dc53c7e5c9bc6a6e
김태현 <taehyk528@gmail.com>
2022-07-25 01:26:55 +0900

    [#15] Alert 익스텐션 추가

---
+++ Tamagotchi/Tamagotchi/Common/UIViewController+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIViewController+Extension.swift
+//  Tamagotchi
+//
+//  Created by taekki on 2022/07/25.
+//
+
+import UIKit
+
+extension UIViewController {
+    func makeAlert(
+        title: String,
+        message: String?,
+        cancelAction: UIAlertAction,
+        confirmAction: UIAlertAction
+    ) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(cancelAction)
+        alertController.addAction(confirmAction)
+        self.present(alertController, animated: true)
+    }
+}
