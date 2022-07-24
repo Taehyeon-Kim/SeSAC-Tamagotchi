9090f1f4dd69fd048e00103dde740410d34976df
김태현 <taehyk528@gmail.com>
2022-07-25 00:31:22 +0900

    [#14] 텍스트 필드 UnderLine 익스텐션

---
+++ Tamagotchi/Tamagotchi/Common/TextField+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  TextField+Extension.swift
+//  Tamagotchi
+//
+//  Created by taekki on 2022/07/25.
+//
+
+import UIKit
+
+extension UITextField {
+    func drawUnderLine(color: UIColor = Color.pointColor){
+        let underLine: CALayer = CALayer()
+        let width: CGFloat = 1.0
+        underLine.frame = CGRect(x: 0, y: self.frame.size.height - width, width:  self.frame.size.width, height: self.frame.size.height)
+        underLine.borderColor = color.cgColor
+        underLine.borderWidth = width
+        self.layer.addSublayer(underLine)
+        self.layer.masksToBounds = true
+    }
+}
