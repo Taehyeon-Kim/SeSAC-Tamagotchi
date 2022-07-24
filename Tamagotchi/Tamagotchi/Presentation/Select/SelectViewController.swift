bd106f2317ef372c4d710ed5cae3633d50f28e24
김태현 <taehyk528@gmail.com>
2022-07-24 18:48:30 +0900

    [#8] 다마고치 셀 연결

--- Tamagotchi/Tamagotchi/Presentation/Select/SelectViewController.swift
+++ Tamagotchi/Tamagotchi/Presentation/Select/SelectViewController.swift
@@ -8,8 +8,34 @@
 import UIKit
 
 final class SelectViewController: UICollectionViewController {
-
+    
+    private let spacing: CGFloat = 20
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureCollectionView()
+    }
+    
+    private func configureCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        let width = UIScreen.main.bounds.width - (spacing * 4)
+        let cellWidth = width / 3
+        layout.itemSize = CGSize(width: cellWidth, height: cellWidth * 1.2)
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        self.collectionView.collectionViewLayout = layout
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: TamagotchiCollectionViewCell.cellID, for: indexPath) as? TamagotchiCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        return cell
     }
 }
