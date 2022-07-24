bdae7b8de73f5aad9700d2cfb2f889eda1755091
김태현 <taehyk528@gmail.com>
2022-07-25 00:35:43 +0900

    [#14] 텍스트 필드 설정

--- Tamagotchi/Tamagotchi/Presentation/Name/NameViewController.swift
+++ Tamagotchi/Tamagotchi/Presentation/Name/NameViewController.swift
@@ -8,8 +8,22 @@
 import UIKit
 
 final class NameViewController: UIViewController {
+    
+    var name: String?
 
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureTextField()
+    }
+}
+
+extension NameViewController {
+    private func configureTextField() {
+        self.name = "고래밥"
+        self.nameTextField.text = name
+        self.nameTextField.textColor = Color.pointColor
+        self.nameTextField.drawUnderLine()
     }
 }
