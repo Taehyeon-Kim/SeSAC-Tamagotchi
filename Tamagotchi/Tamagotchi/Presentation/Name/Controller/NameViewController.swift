5b031e9acf8de1b58910ccf7326741dd2c6ca93b
김태현 <taehyk528@gmail.com>
2022-07-25 20:56:29 +0900

    [#30] 이름 변경 화면 코드 정리

--- Tamagotchi/Tamagotchi/Presentation/Name/Controller/NameViewController.swift
+++ Tamagotchi/Tamagotchi/Presentation/Name/Controller/NameViewController.swift
@@ -9,29 +9,39 @@ import UIKit
 import Toast
 
 final class NameViewController: UIViewController {
-    
-    var name: String?
+ 
+    // MARK: - Outlets
 
     @IBOutlet weak var nameTextField: UITextField!
     
+    
+    // MARK: - Life Cycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.configureTextField()
-        self.configureNavigationBar()
+        
+        self.configureUI()
     }
 }
 
+// MARK: - Methods
+
 extension NameViewController {
+    
+    private func configureUI() {
+        self.configureTextField()
+        self.configureNavigationBar()
+    }
+    
     private func configureTextField() {
-        self.name = UserDefaultManager.nickname
-        self.nameTextField.placeholder = "\(name!)님 이름을 입력해주세요!"
-        self.nameTextField.text = name
+        self.nameTextField.placeholder = "\(UserDefaultManager.nickname)님 이름을 입력해주세요!"
+        self.nameTextField.text = UserDefaultManager.nickname
         self.nameTextField.textColor = Color.pointColor
         self.nameTextField.drawUnderLine()
     }
     
     private func configureNavigationBar() {
-        self.title = "\(name ?? "대장")님의 이름 정하기"
+        self.title = "\(UserDefaultManager.nickname)님의 이름 정하기"
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "저장", style: .plain, target: self, action: #selector(saveButtonTapped))
     }
     
