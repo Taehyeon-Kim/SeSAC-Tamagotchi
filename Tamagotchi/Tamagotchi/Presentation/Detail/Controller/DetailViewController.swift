0d1959de336a1436f2128cd6216462ab64f5e7a2
김태현 <taehyk528@gmail.com>
2022-07-25 20:01:48 +0900

    [#18] 상세 팝업 화면 코드 정리

--- Tamagotchi/Tamagotchi/Presentation/Detail/Controller/DetailViewController.swift
+++ Tamagotchi/Tamagotchi/Presentation/Detail/Controller/DetailViewController.swift
@@ -9,8 +9,12 @@ import UIKit
 
 final class DetailViewController: UIViewController {
     
+    // MARK: - Properties
+    
     var type: SelectSceneState?
     var tamagotchi: Tamagotchi?
+    
+    // MARK: - Outlets
 
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var profileImageView: UIImageView!
@@ -19,11 +23,16 @@ final class DetailViewController: UIViewController {
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var startButton: UIButton!
     
+    // MARK: - Life Cycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.configureUI()
         self.configureData()
     }
+    
+    // MARK: - Actions
     
     @IBAction func cancelButtonTapped(_ sender: UIButton) {
         self.dismiss(animated: true)
@@ -31,25 +40,18 @@ final class DetailViewController: UIViewController {
     
     @IBAction func startButtonTapped(_ sender: UIButton) {
         self.saveData()
-        let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
-        let sceneDelegate = windowScene?.delegate as? SceneDelegate
-        guard let mainViewController = StoryboardManager.instantiateViewController(.main, for: MainViewController.self) else { return }
-        sceneDelegate?.window?.rootViewController = UINavigationController(rootViewController: mainViewController)
-        sceneDelegate?.window?.makeKeyAndVisible()
+        self.transitionToMainViewController()
     }
 }
 
+// MARK: - Methods
+
 extension DetailViewController {
+    
     private func configureUI() {
         self.configureContainerView()
         self.configureButtons()
         self.configureLabels()
-    }
-    
-    private func configureData() {
-        self.profileImageView.image = tamagotchi?.getInitialProfileImage()
-        self.nameLabel.text = tamagotchi?.name
-        self.descriptionLabel.text = tamagotchi?.description
     }
     
     private func configureContainerView() {
@@ -58,25 +60,42 @@ extension DetailViewController {
     }
     
     private func configureLabels() {
-        self.nameLabel.padding(top: 4, bottom: 4, left: 6, right: 6)
         self.nameLabel.font = .boldSystemFont(ofSize: 15)
         self.nameLabel.textColor = Color.pointColor
+        self.nameLabel.padding(top: 4, bottom: 4, left: 6, right: 6)
         self.nameLabel.makeRoundedWithBorder(radius: 4, color: Color.pointColor)
-        self.descriptionLabel.textColor = Color.pointColor
+        
         self.descriptionLabel.font = .systemFont(ofSize: 13)
+        self.descriptionLabel.textColor = Color.pointColor
     }
 
     private func configureButtons() {
         self.startButton.setTitle(type?.buttonTitle, for: .normal)
-        self.cancelButton.setTitleColor(Color.pointColor, for: .normal)
         self.startButton.setTitleColor(Color.pointColor, for: .normal)
-        self.cancelButton.makeBorder(color: Color.pointColor, borderWith: 0.5)
         self.startButton.makeBorder(color: Color.pointColor, borderWith: 0.5)
+        
+        self.cancelButton.setTitleColor(Color.pointColor, for: .normal)
+        self.cancelButton.makeBorder(color: Color.pointColor, borderWith: 0.5)
+    }
+    
+    private func configureData() {
+        self.profileImageView.image = tamagotchi?.getInitialProfileImage()
+        self.nameLabel.text = tamagotchi?.name
+        self.descriptionLabel.text = tamagotchi?.description
     }
     
     private func saveData() {
         UserDefaultManager.isAppFirstLaunch = false
         UserDefaultManager.characterType = tamagotchi?.type.rawValue ?? 0
         UserDefaultManager.characterName = tamagotchi?.name ?? ""
+    }
+    
+    private func transitionToMainViewController() {
+        let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        let sceneDelegate = windowScene?.delegate as? SceneDelegate
+        
+        guard let mainViewController = StoryboardManager.instantiateViewController(.main, for: MainViewController.self) else { return }
+        sceneDelegate?.window?.rootViewController = UINavigationController(rootViewController: mainViewController)
+        sceneDelegate?.window?.makeKeyAndVisible()
     }
 }
